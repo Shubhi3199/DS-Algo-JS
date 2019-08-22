bf4474090d46e574fc97c5625d93ca467697d5de
shubhi3199 <shbhm3199@gmail.com>
2019-08-22 08:18:07 +0530

    Initial Commit

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+    for(let i=0;i<n;i++){
+        let stair='';
+        for(let j=0;j<n;j++){
+            if(i>=j){
+                stair+='#';
+            }else{
+                stair+=' ';
+            }
+
+        }
+        console.log(stair);
+    }
+
+}
+
 
 module.exports = steps;
