14d958168db70d1e7ff4f0e2719b47d3b4eba0e9
shubhi3199 <shbhm3199@gmail.com>
2019-08-22 16:28:15 +0530

    Initial Commit

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,22 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-
-    for(let i=0;i<n;i++){
-        let stair='';
-        for(let j=0;j<n;j++){
-            if(i>=j){
-                stair+='#';
-            }else{
-                stair+=' ';
-            }
-
-        }
-        console.log(stair);
+function steps(n, row=0, stair='') {
+    if(n===row){
+        return;
     }
 
+        if(stair.length===n){
+            console.log(stair);
+            return steps(n,row+1);
+        }
+
+        if(stair.length<= row){
+            stair +='#';
+        }else{
+            stair +=' ';
+        }
+        steps(n, row, stair);
+
 }
+
+
+//
+// function steps(n) {
+//
+//     for(let i=0;i<n;i++){
+//         let stair='';
+//         for(let j=0;j<n;j++){
+//             if(i>=j){
+//                 stair+='#';
+//             }else{
+//                 stair+=' ';
+//             }
+//
+//         }
+//         console.log(stair);
+//     }
+//
+// }
 
 
 module.exports = steps;
