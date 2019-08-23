322d2b7b1ea6b5f7cbee5eab24e78872f14ab42d
shubhi3199 <shbhm3199@gmail.com>
2019-08-23 07:15:08 +0530

    Initial Commit

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    let midPoint=Math.floor((2*n-1)/2);
+    for(let row=0;row<n;row++){
+        let level='';
+        for(let column=0;column<2*n-1;column++){
+            if(midPoint-row<=column && midPoint+row>=column){
+                level +='#';
+            }else {
+                level +=' ';
+            }
+        }
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
