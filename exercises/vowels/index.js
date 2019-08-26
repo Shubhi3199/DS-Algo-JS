62e141809151c451fe753abc14103dd290f6563e
shubhi3199 <shbhm3199@gmail.com>
2019-08-27 01:03:24 +0530

    Solution using Regular expression

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,14 +8,21 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let count=0;
-    let checker=['a', 'e', 'i', 'o', 'u'];
-    for(let char of str.toLowerCase()){
-       if(checker.includes(char)){
-           count++;
-       }
-    }
-   return count;
-}
+   const matches=str.match(/[aeiou]/gi);   //Here match is method on regular expression which returns us an array with a
+   if(matches){                            //length equals to the no. of vowels in the given str String.
+       return matches.length;              //g=>check the complete str and do not stop checking when the first vowel
+   }else return  0;                        //is found.
+}                                          //i=>to insure it is case insensitive.
 
 module.exports = vowels;
+//
+// function vowels(str) {
+//     let count=0;
+//     let checker=['a', 'e', 'i', 'o', 'u'];
+//     for(let char of str.toLowerCase()){
+//         if(checker.includes(char)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
